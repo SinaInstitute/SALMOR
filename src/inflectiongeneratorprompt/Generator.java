abf1723fc62ae40223053658d2137a75fdf694a5
SinaInstitute <bzusinainstitute@gmail.com>
2015-06-18 12:35:10 +0300

    initial commit

---
+++ src/inflectiongeneratorprompt/Generator.java
@@ -0,0 +1,22 @@
+/*
+ * Created by Rami Asia
+ * This software is property of the Sina Institute
+ */
+package inflectiongeneratorprompt;
+
+/**
+ *
+ * @author rami
+ */
+public class Generator implements Runnable{
+    
+    public Generator(){
+        
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
